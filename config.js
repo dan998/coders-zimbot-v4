4533be7e121f4a73f5e31bf59cd45325811127f7
lloyd4565 <88751177+lloyd4565@users.noreply.github.com>
2022-06-16 14:12:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','918188019676']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
+global.owner = ['27721753314']
+global.premium = ['27721753314']
+global.botname = 'HIDEKI YASHIDA'
+global.ownername = 'MR LLOYD'
 global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
 global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
 global.packname = 'ZIM BOT INC'
